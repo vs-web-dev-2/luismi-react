f8f0ae34d4cf59fc837dec2a276a1310a227bd1c
Luis Miguel <glmg69@gmail.com>
2021-06-25 13:11:23 +0200

    divisas +

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 import ListaDivisas from './components/ListaDivisas';
+import { useState } from 'react';
 
 function App() {
-  const nombre = 'Luismi';
+
+  const [nombre, setNombre] = useState('Luismi');
+  const [vistavisible, setVistaVisible] = useState(false);
+
+  const cambiaVisibilidadLista = () => {
+    setVistaVisible(!vistavisible);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hola {nombre}</h1>
-        <ListaDivisas frase="Lista de divisas" />
+        <button onClick={cambiaVisibilidadLista}>Muestra / oculta Listado</button>
+        {
+          vistavisible &&
+          <ListaDivisas frase="Lista de divisas" />
+        }
       </header>
     </div>
   );
--- src/components/Divisa.js
+++ src/components/Divisa.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import './Divisa.css';
 
 class Divisa extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            resaltado: false
+        }
+        this.handlerClick = this.handlerClick.bind(this);
     }
+
+    handlerClick() {
+        const nuevoResaltado = !this.state.resaltado;
+        this.setState({ resaltado: nuevoResaltado })
+    }
+
     render() {
         return (
-            <li>
+            <li
+                className={this.state.resaltado ? 'resaltado' : ''}
+                onClick={this.handlerClick}>
                 {this.props.nombre}: {this.props.valor}   {this.props.valorMenos1}
             </li>
         );
